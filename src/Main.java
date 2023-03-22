e54e31c2531fca2ea1cb1c9f336a0e2940116827
AndyDig <126315776+AndyDig@users.noreply.github.com>
2023-03-23 01:04:00 +0300

    DZ3.12

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 67;
+        if ((a % 2) == 0) {
+            System.out.println("Четное");
+        } else {
+            System.out.println("Нечетное");
+        }
+    }
+}
+
